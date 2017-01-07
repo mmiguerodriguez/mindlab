bdb9f772759a5e1892ec29c2a25b3643161df41b
Joel Sobol Mark <joelsobolmark@gmail.com>
2017-01-07 19:24:21 -0300

    Started the design of LessonsList

--- imports/ui/CoursePage/LessonItem/LessonItem.jsx
+++ imports/ui/CoursePage/LessonItem/LessonItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 const LessonItem = ({ name, index }) => {
   return (
-    <p>
-      {`${name} -> ${index}`}
-    </p>
+    <div className="lesson-item">
+      <Link to="/course/programacion" className="btn">
+        <h3>
+          {`${index}. ${name}`}
+        </h3>
+      </Link>
+      <hr />
+    </div>
   );
 };
 
