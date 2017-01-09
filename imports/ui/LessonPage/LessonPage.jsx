4911d4ae9f60d422cde41ee5bfd0cf79185149bf
Joel Sobol Mark <joelsobolmark@gmail.com>
2017-01-09 19:26:13 -0300

    Started lessonPage structure

--- imports/ui/LessonPage/LessonPage.jsx
+++ imports/ui/LessonPage/LessonPage.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 class LessonPage extends React.Component {
   render() {
-    return (null);
+    // hardcoded way to get the lessons
+    const lessonsContent = [
+      
+    ];
+    return (
+      <div>
+        {this.props.params.lessonName}
+      </div>
+    );
   }
 }
+
+LessonPage.propTypes = {
+  params: React.PropTypes.shape({
+    lessonName: React.PropTypes.string.isRequired,
+  }),
+};
 
 export default LessonPage;
