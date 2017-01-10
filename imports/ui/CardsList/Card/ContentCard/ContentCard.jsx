1166f772ff8506dae57d3beb2babd83a9ac64269
Joel Sobol Mark <joelsobolmark@gmail.com>
2017-01-10 11:44:38 -0300

    Finished ContentCard

--- imports/ui/CardsList/Card/ContentCard/ContentCard.jsx
+++ imports/ui/CardsList/Card/ContentCard/ContentCard.jsx
@@ -2,14 +2,38 @@ import React from 'react';
 
 import Card from '../Card';
 
-class ContentCard extends React.Component {
-  render() {
-    const content =
-      <p>holas</p>
-    return (
-      <Card content={content} />
+const ContentCard = ({ imageUrl, title, text }) => {
+  const content =
+    (
+      <div className="content-card">
+        { imageUrl &&
+          <img
+            src={imageUrl}
+            alt=""
+            className="content-card-img"
+          />
+        }
+        { title &&
+          <h2 className="content-card-title">
+            {title}
+          </h2>
+        }
+        { text &&
+          <h3 className="content-card-text">
+            {text}
+          </h3>
+        }
+      </div>
     );
-  }
-}
+  return (
+    <Card content={content} />
+  );
+};
+
+ContentCard.propTypes = {
+  imageUrl: React.PropTypes.string,
+  title: React.PropTypes.string,
+  text: React.PropTypes.string,
+};
 
 export default ContentCard;
