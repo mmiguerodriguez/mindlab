aa1cdcb765ec1cab17fa1f2acac59da192491fad
Santiago Aranguri <santiago.aranguri@gmail.com>
2017-01-18 13:35:38 -0300

    Solved some merge conflicts

--- client/main.jsx
+++ client/main.jsx
@@ -11,11 +11,10 @@ import { ApolloProvider } from 'react-apollo';
 import App from '../imports/ui/App/App';
 import WelcomePage from '../imports/ui/WelcomePage/WelcomePage';
 import HomePage from '../imports/ui/HomePage/HomePage';
-import CardsList from '../imports/ui/CardsList/CardsList';
+import NotificationsTest from '../imports/ui/NotificationsTest';
 import CoursePage from '../imports/ui/CoursePage/CoursePage';
 import LessonPage from '../imports/ui/LessonPage/LessonPage';
 import FeedbackPage from '../imports/ui/FeedbackPage/FeedbackPage';
-import NotificationsTest from '../imports/ui/NotificationsTest';
 
 import './main.scss';
 
@@ -38,7 +37,6 @@ Meteor.startup(() => {
           <IndexRoute component={HomePage} />
           <Route path="/course/:courseName" component={CoursePage} />
           <Route path="/course/:courseName/:lessonName" component={LessonPage} />
-          <Route path="/lesson" component={CardsList} />
           <Route path="/feedback" component={FeedbackPage} />
           <Route path="/test" component={NotificationsTest} />
         </Route>
