90484943007ff9a5949d03215668ad8e4047024b
Nic Szer <Nic Szer>
2017-01-19 03:01:08 -0300

    Applied SlideHelper to Card

--- imports/ui/CardsList/Card/Card.jsx
+++ imports/ui/CardsList/Card/Card.jsx
@@ -1,18 +1,87 @@
 import React from 'react';
+import Measure from 'react-measure';
+import $ from 'jquery';
+import SlideHelper from './SlideHelper';
 
 class Card extends React.Component {
 
   // TODO: implement slideHelper and pass cardPassed callback
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dimensions: {
+        width: 1, // Default value only
+        height: 1, // Default value only
+        measured: false,
+      },
+      displacement: {
+        x: 0, // Used to animate card movement
+      },
+    };
+    this.cardSliderApplied = false;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (!this.cardSliderApplied) {
+      return true;
+    }
+    if (nextState.displacement.x === this.state.displacement.x) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Updates or sets a sliding helper for the cards.
+   * WARNING:
+   *  Requires dimensions to be measured
+   * @return {[type]} [description]
+   */
+  updateCardSlider() {
+    if (!this.cardSlider) {
+      // Create and instantiate a SlideHelper
+      const $card = $(this.card);
+      const stateUpdateHandler = (stateX) => {
+        this.setState({
+          displacement: {
+            x: stateX * this.state.dimensions.width,
+          },
+        });
+      };
+      const slideHelperProps = {
+        $element: $card,
+        size: this.state.dimensions.width,
+        finishHandler: this.props.cardPassed,
+        stateUpdateHandler,
+      };
+      this.cardSlider = new SlideHelper(slideHelperProps);
+    } else {
+      this.cardSlider.setSize(this.state.dimensions.width);
+    }
+  }
+
   render() {
+    if (this.state.dimensions.measured) {
+      this.updateCardSlider();
+      this.cardSliderApplied = true;
+    }
+
     const cardStyle = {
       zIndex: this.props.cardsCount - this.props.index,
       transform: this.props.cardsCount ?
-        `translateY(${10 * (this.props.index)}px)` : null,
+        `translate(${this.state.displacement.x}px, ${10 * (this.props.index)}px) rotateZ(${(this.state.displacement.x / this.state.dimensions.width) * 35}deg)` : null,
     };
     return (
-      <div className="_card" style={cardStyle}>
-        {this.props.content}
-      </div>
+      <Measure
+        onMeasure={(dimensions) => {
+          this.setState({ dimensions: { ...dimensions, measured: true } });
+        }}
+      >
+        <div className="_card" style={cardStyle} ref={(card) => { this.card = card; }}>
+          {this.props.content}
+        </div>
+      </Measure>
     );
   }
 }
