8160088500578017b7897b30a1dc2221c42a91a6
Joel Sobol Mark <joelsobolmark@gmail.com>
2017-01-23 11:26:50 -0300

    Reverted testing changes in LessonPage

--- imports/ui/LessonPage/LessonPage.jsx
+++ imports/ui/LessonPage/LessonPage.jsx
@@ -9,14 +9,71 @@ class LessonPage extends React.Component {
     // TODO: add real cards
     const cardsContent = [
       {
-        type: 'code',
-        task: 'Devolvé la suma de los números del 1 al 100',
-        results: [
-          {
-            result: '5050',
-            correct: true,
-          },
-        ],
+        type: 'multiple-choice',
+        imageUrl: '/images/welcome/page1.png',
+        question: 'Tercera card',
+        options: [{
+          content: 'cantidadApariciones',
+          correct: true,
+        },
+        {
+          content: 'vecesQueAparece',
+        },
+        {
+          content: 'numero',
+        }],
+      },
+      {
+        type: 'order',
+        question: 'Tercera card',
+        options: [{
+          content: 'Enchufar licuadora',
+          step: 1,
+        },
+        {
+          content: 'Meter ingredientes en la licuadora',
+          step: 2,
+        },
+        {
+          content: 'Licuar',
+          step: 3,
+        },
+        {
+          content: 'Servir licuado en el vaso',
+          step: 4,
+        },
+        {
+          content: 'Tomar el licuado',
+          step: 5,
+        }],
+      },
+      {
+        type: 'content',
+        imageUrl: '/images/welcome/page1.png',
+        title: 'HOLA',
+        text: 'Esto es un texto',
+      },
+      {
+        type: 'content',
+        imageUrl: '/images/welcome/page2.png',
+        title: 'Segunda card',
+        text: 'Esto es otro texto, un poco más largo que el anterior',
+      },
+      {
+        type: 'content',
+        imageUrl: '/images/welcome/page1.png',
+        title: 'Tercera card',
+        text: 'Esto es otro texto, un poco más largo que el anterior, y también que el primero.',
+      },
+      {
+        type: 'order',
+      },
+      {
+        type: 'multiple-choice',
+      },
+      {
+        type: 'content',
+        title: 'nuevo stack',
       },
     ];
     return (
